74f9bb34fa11bfadb4cf6150a37ae60eb9860139
Vlad Keliy <limonkeliy153@gmail.com>
2020-01-02 20:34:06 +0300

    :sparkles: Add new url post endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,33 +1,70 @@
 import express from 'express';
 import path from 'path';
 import cors from 'cors';
-// import mongoose from 'mongoose';
-// import mongo from 'mongodb';
+import bodyParser from 'body-parser';
+import dns from 'dns';
+import mongoose from 'mongoose';
+import url from 'url';
+import dotenv from 'dotenv';
+
+const dnsPromises = dns.promises;
 
 const app = express();
+dotenv.config();
 
-// Basic Configuration
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-/** this project needs a db !! **/
-// mongoose.connect(process.env.MONGOLAB_URI);
+const MONGO_DB_LOGIN = process.env.MONGO_DB_LOGIN;
+const MONGO_DB_PASSWORD = process.env.MONGO_DB_PASSWORD;
+const MONGO_CLUSTER_ADDRESS = process.env.MONGO_CLUSTER_ADDRESS;
+
+const MONGO_DB_URL = `mongodb+srv://${MONGO_DB_LOGIN}:${MONGO_DB_PASSWORD}@${MONGO_CLUSTER_ADDRESS}.mongodb.net/test?retryWrites=true&w=majority`;
+
+const urlSchema = new mongoose.Schema({
+  originalUrl: String,
+});
+
+const Urls = mongoose.model('Urls', urlSchema);
 
 app.use(cors());
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+mongoose.connect(MONGO_DB_URL, { useNewUrlParser: true }, err => {
+  if (err) return console.log(err);
 
-/** this project needs to parse POST bodies **/
-// you should mount the body-parser here
+  app.listen(PORT, () => console.log('Node.js listening ...'));
+});
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/views/index.html'));
 });
 
-// your first API endpoint...
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
+const getShortUrl = async (originalUrl: string): Promise<mongoose.Document> => {
+  const sameUrls = await Urls.find({ originalUrl });
 
-app.listen(port, function() {
-  console.log('Node.js listening ...');
+  if (sameUrls.length !== 0) {
+    return sameUrls[0];
+  }
+
+  const newUrl = new Urls({ originalUrl });
+  await newUrl.save();
+
+  return newUrl;
+};
+
+app.post('/api/shorturl/new', async (req, res) => {
+  try {
+    const originalUrl = new url.URL(req.body?.url);
+    const hostname = originalUrl.hostname;
+    const originalHref = originalUrl.href;
+
+    await dnsPromises.lookup(hostname);
+    const shortUrl = await getShortUrl(originalHref);
+
+    // eslint-disable-next-line @typescript-eslint/camelcase
+    res.json({ original_url: originalHref, short_url: shortUrl._id });
+  } catch {
+    res.json({ error: 'invalid URL' });
+  }
 });
